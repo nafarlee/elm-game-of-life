81b0169703f632b63b19a8b3dca5b6a133f7e66d
Nicholas Farley <nicholasafarley@gmail.com>
2017-05-17 01:25:17 -0400

    Add Cardinal.findNeighbors function

--- src/Cardinal.elm
+++ src/Cardinal.elm
@@ -1,6 +1,9 @@
 module Cardinal exposing (..)
 
 
+import Set exposing (Set)
+
+
 type alias Coordinate = (Int, Int)
 
 
@@ -10,3 +13,20 @@ type alias Bounds =
     , width : Int
     , height : Int
     }
+
+
+findNeighbors : Coordinate -> Set(Coordinate)
+findNeighbors coord =
+    let
+        (x, y) = coord
+    in
+        Set.fromList
+          [ ( x + 1, y + 1 )
+          , ( x + 1, y )
+          , ( x + 1, y - 1 )
+          , ( x, y + 1 )
+          , ( x, y - 1 )
+          , ( x - 1, y + 1 )
+          , ( x - 1, y )
+          , ( x - 1, y - 1 )
+          ]
