f36292090be2247766326c2b2411c9245c21f32b
Nicholas Farley <nicholasafarley@gmail.com>
2017-05-17 01:41:35 -0400

    Define Dirty reducer in terms of Cardinal.findNeighbors

--- src/Dirty.elm
+++ src/Dirty.elm
@@ -12,20 +12,4 @@ find alive bounds =
 
 reducer : Coordinate -> Set(Coordinate) -> Set(Coordinate)
 reducer current accumulated =
-  let
-      (x, y) = current
-      new = Set.fromList
-        [ (x + 1, y + 1)
-        , (x + 1, y)
-        , (x + 1, y - 1)
-        , (x, y + 1)
-        , (x, y - 1)
-        , (x - 1, y + 1)
-        , (x - 1, y)
-        , (x - 1, y - 1)
-        ]
-  in
-     Set.union accumulated new
-
-
-predicate : 
+     Set.union (Cardinal.findNeighbors current) accumulated
