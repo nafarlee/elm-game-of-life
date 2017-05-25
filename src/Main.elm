073823a9c53a8dcf5976be8b7a0e6d0dfaeafaa0
Nicholas Farley <nicholasafarley@gmail.com>
2017-05-24 23:36:17 -0400

    Retemplate main

--- src/Main.elm
+++ src/Main.elm
@@ -1,73 +1,39 @@
 import Time exposing (Time,second)
-import Html exposing (Html,table,tr,td)
-import Html.Attributes exposing(style,height,width)
-import Random
-
-import Matrix exposing (Matrix)
+import Dict exposing (Dict)
 
 import Conway exposing (Cell(Alive,Dead))
-import Neighbors exposing (neighborMap)
 
 
 -- Model
-type alias Model = Matrix Cell
+type alias Coordinate = (Int, Int)
+type alias Board = Dict Coordinate Cell
 
 
-cell : Bool -> Cell
-cell b =
-    if b then
-        Alive
-    else
-        Dead
+type alias Model =
+    { board: Board
+    }
 
 
 init : (Model, Cmd Msg)
 init =
-    let
-        size = 100
-        matrix = Matrix.square size <| always Dead
-        matrixGenerator
-            = Random.bool
-            |> Random.map cell
-            |> Random.list size
-            |> Random.list size
-            |> Random.map Matrix.fromList
-    in
-        (matrix, Random.generate Genesis matrixGenerator)
 
 
 -- Messages
 type Msg
     = Tick Time
-    | Genesis (Matrix Cell)
+    | Genesis (Board)
 
 
 -- View
 view : Model -> Html Msg
-view matrix =
-    let
-        innerMap = \cell ->
-            case cell of
-                Alive -> td [style [("backgroundColor", "black")]] []
-                Dead -> td [style [("backgroundColor", "white")]] []
-
-        outerMap = \row ->
-            tr [] <| List.map innerMap row
-    in
-        table
-            [ style
-                [ ("table-layout", "fixed")
-                , ("height", "800px")
-                , ("width", "800px")
-                ]
-            ] <| List.map outerMap <| Matrix.toList matrix
+view { board } =
 
 
 -- Update
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
-        Genesis matrix -> (matrix, Cmd.none)
+        Genesis board -> (board, Cmd.none)
         Tick _ -> (neighborMap Conway.apply model, Cmd.none)
 
 
